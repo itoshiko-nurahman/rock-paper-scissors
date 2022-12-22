2f4c434e34a6bb4ebb1c27e205d5868d1b6f4bce
Itoshiko Nurahman <itoshikonurahman@gmail.com>
2022-12-22 20:48:29 +0700

    Remove uncesessary file

--- script.js
+++ script.js
@@ -14,18 +14,45 @@ function getComputerChoice() {
   }
 }
 
-const computerChoice = getComputerChoice();
-//Make input from user
-let playerChoice = prompt('Please enter your choice').toLowerCase();
+
 
 
 //Make function to get the winner
 function playRound(player, computer) {
   if (player == computer) return 'Draw!';
-  if (player == 'rock' ) return (computer == 'scissors') ? 'Player Win' : 'Computer Win!';
+  if (player == 'rock' ) return (computer == 'scissors') ? 'Player Win!' : 'Computer Win!';
   if (player == 'paper') return (computer == 'rock') ? 'Player Win!' : 'Computer Win!';
   if (player == 'scissors') return (computer == 'paper') ? 'Player Win!' : 'Computer Win!';
   }
 
+//Make variable to save the score
+let playerScore = 0;
+let computerScore = 0;
 
-//Don't know how to make score tracker
+//Make score tracker with 5 rounds game
+for ( let i = 0; i < 5; i++ ) {
+  const computerChoice = getComputerChoice();
+  //Make input from user
+  let playerChoice = prompt('Pilih antara rock, paper, scissors').toLowerCase();
+  const result = playRound(playerChoice, computerChoice);
+  if (result === 'Player Win!') {
+    playerScore++;
+  } else if (result === 'computer Win!') {
+    computerScore ++;
+  }
+
+  //Show the winner of this round
+  console.log(`Pemenang ronde ${i + 1} adalah ${result}`);
+}
+
+//Show the final score
+console.log(`Skor akhir permainan ini adalah : Pemain = ${playerScore}, Komputer = ${computerScore}`);
+
+//Pick the winner of the game
+if ( playerScore > computerScore ) {
+  console.log("Pemenang permainan ini adalah Player, selamat karena mengalahkan komputer");
+} else if ( playerScore < computerScore) {
+  console.log("Oh tidak, Komputer menang. Sepertinya manusia kalah sekarang");
+} else {
+  console.log("Seri!!");
+}
